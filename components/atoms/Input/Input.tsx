9eaf28547df2d2b1ed6a5d8ef14e45073da49bcb
JACisneros94 <jacisnerosp.94@gmail.com>
2025-02-05 17:11:54 -0600

    Layout Modal facturas, sum facturas & modify Input component

--- components/atoms/Input/Input.tsx
+++ components/atoms/Input/Input.tsx
@@ -1,7 +1,8 @@
 import { FC } from "react";
 
 export interface InputProps {
-  key: number;
+  checked?: boolean;
+  key?: number;
   nombre: string;
   requerido: boolean;
   placeHolder?: string;
@@ -16,25 +17,26 @@ export interface InputProps {
 }
 
 export const Input: FC<InputProps> = (props) => {
-  const { nombre, funcion, placeHolder, requerido, type, valor, isEdited = false } = props;
+  const { checked, nombre, funcion, placeHolder, requerido, type, valor, isEdited = false } = props;
 
   return (
-    <div className="inputContainer relative">
+    <div className={(type !== 'checkbox' ? "inputContainer relative" : "flex items-center")}>
       <input
         type={type}
-        className="peer h-12 w-full rounded-lg  ring-1 px-2 ring-gray-300 focus:ring-blue-600 focus:outline-none focus:border-blue-600"
+        className={(type !== 'checkbox' ? "peer h-12 w-full rounded-lg  ring-1 px-2 ring-gray-300 focus:ring-blue-600 focus:outline-none focus:border-blue-600" : "")}
         name={nombre}
         placeholder={placeHolder}
         required={requerido}
         value={valor}
         onChange={(e) => funcion(nombre, e.target.value)}
         disabled={isEdited}
+        checked={ type === 'checkbox' ? checked : undefined }
       />
       <label
-        htmlFor={nombre}
-        className="absolute cursor-text left-0 -top-3 text-sm text-gray-400 mx-1 px-1 peer-placeholder-shown:-z-10 peer-placeholder-shown:text-base peer-placeholder-shown:text-[#6f6d74] peer-placeholder-shown:top-3 peer-focus:z-10 peer-focus:-top-3 bg-[#ffffff]"
+        htmlFor={placeHolder}
+        className={(type !== 'checkbox' ? "absolute cursor-text left-0 -top-3 text-sm text-gray-400 mx-1 px-1 peer-placeholder-shown:-z-10 peer-placeholder-shown:text-base peer-placeholder-shown:text-[#6f6d74] peer-placeholder-shown:top-3 peer-focus:z-10 peer-focus:-top-3 bg-[#ffffff]": "ml-2")}
       >
-        {nombre}
+        {placeHolder || nombre}
       </label>
     </div>
   );
