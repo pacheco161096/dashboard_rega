62df30b0e923335b5ea0c18c62a8e3eebd17aa4b
JACisneros94 <jacisnerosp.94@gmail.com>
2025-03-05 18:50:35 -0600

    Counter quantity, remove package on the car & blocked button Pay

--- app/cobranza/page.tsx
+++ app/cobranza/page.tsx
@@ -12,6 +12,7 @@ function Cobranza() {
   const [user, setUser] = useState<User | null>(null);
   const [carShop, setCarShop] = useState([])
   const [currentDate, setCurrentDate] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState("Efectivo");
 
   useEffect(() => {
     const selectedUser = sessionStorage.getItem("selectedUser");
@@ -26,7 +27,7 @@ function Cobranza() {
       }
     }
 
-    const today = new Date().toISOString().split("T")[0]; // Obtener la fecha en formato YYYY-MM-DD
+    const today = new Date().toISOString().split("T")[0];
     setCurrentDate(today);
   }, []);
 
@@ -58,6 +59,24 @@ function Cobranza() {
         return [...prevCarShop, { ...item, cantidad: 1 }];
       }
     });
+  };
+
+  const incrementQuantity = (id: string) => {
+    setCarShop((prevCarShop) =>
+      prevCarShop.map((item) =>
+        item.id === id ? { ...item, cantidad: item.cantidad + 1 } : item
+      )
+    );
+  };
+  
+  const decrementQuantity = (id: string) => {
+    setCarShop((prevCarShop) =>
+      prevCarShop
+        .map((item) =>
+          item.id === id ? { ...item, cantidad: item.cantidad - 1 } : item
+        )
+        .filter((item) => item.cantidad > 0) // Elimina si la cantidad llega a 0
+    );
   };
 
   function openDrawerVenta() {
@@ -127,27 +146,25 @@ function Cobranza() {
         </div>
         {/* Sidebar */}
         <div className={s["Cobranza-sidebar"]}>
+          <h1 className="text-center font-semibold mb-3">
+            Movimientos del { currentDate }
+          </h1>
           <div className={s["Cobranza-titleSide"]}>
-            <h2 className="text-lg font-semibold">
-              Productos
-            </h2>
-            <a href="#" className="text-blue-500 text-sm underline">
-              Vaciar canasta
-            </a>
+            <h3 className="font-semibold">
+              Ventas
+            </h3>
           </div>
-          <div className={s["Cobranza-cart"]}>
-            <span className="text-5xl">🛒</span>
-            <span className="text-md font-semibold">
-              No hay conceptos
-            </span>
+          <ul className={s["Cobranza-list"]}>
+            <li>Concepto</li>
+          </ul>
+          <div className={s["Cobranza-titleSide"]}>
+            <h3 className="font-semibold">
+              Gastos
+            </h3>
           </div>
-          <div className={s["Cobranza-footer"]}>
-            <span>0</span>
-            <span>
-              Continuar
-            </span>
-            <span className="font-bold">$0</span>
-          </div>
+          <ul className={s["Cobranza-list"]}>
+            <li>Concepto</li>
+          </ul>
         </div>
       </div>
       
@@ -232,41 +249,54 @@ function Cobranza() {
                 </div>}
               </div>
             </div>
-            {carShop.length > 0 && <div className="flex flex-col mt-10 bg-gray-700 p-2 rounded-lg">
-              <div className="flex gap-1 mb-2"> 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                </svg>
-                <h1 className="text-xl">Carrito</h1>
-              </div>
-              <div className="grid grid-cols-3 font-semibold">
-                <span>Nombre</span>
-                <span>Precio</span>
-                <span>Cantidad</span>
-              </div>
-              {carShop?.map(item => <div key={item?.id} className="grid grid-cols-3 text-xs my-2">
-                <span>{item?.titulo}</span>
-                <span>${item?.precio}</span>
-                <div className="grid grid-cols-3">
-                  <div className="font-bold">-</div>
-                  <div>{item?.cantidad}</div>
-                  <div className="font-bold">+</div>
+            {
+              carShop.length > 0 &&
+              <div className="flex flex-col mt-7 bg-gray-700 p-2 rounded-lg">
+                <div className="flex gap-1 mb-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                  </svg>
+                  <h1 className="text-xl">Carrito</h1>
                 </div>
-              </div>)}  
-              <div className="border-t border-gray-800 flex justify-end py-2">
-                <div><span className="font-bold">Total:</span> ${totalPrecio}</div>
+                <div className="grid grid-cols-3 font-semibold">
+                  <span>Nombre</span>
+                  <span>Precio</span>
+                  <span>Cantidad</span>
+                </div>
+                {
+                  carShop?.map(item =>
+                    <div key={item?.id} className="grid grid-cols-3 text-xs my-2">
+                      <span>{item?.titulo}</span>
+                      <span>${item?.precio}</span>
+                      <div className="grid grid-cols-3">
+                        <button className={s["Cobranza-buttonCounter"]} onClick={ () => decrementQuantity(item.id) }>
+                          -
+                        </button>
+                        <div className="text-center">
+                          {item?.cantidad}
+                        </div>
+                        <button className={s["Cobranza-buttonCounter"]} onClick={ () => incrementQuantity(item.id) }>
+                          +
+                        </button>
+                      </div>
+                    </div>)
+                }
+                <div className="border-t border-gray-800 flex justify-end py-2">
+                  <div><span className="font-bold">Total:</span> ${totalPrecio}</div>
+                </div>
               </div>
-            </div>}
-            {/* <label className="block mt-4 text-gray-400 text-sm">
-              Monto
+            }
+            <label className="block mt-4 text-gray-400 text-sm" htmlFor="payment-method">
+              Método de Pago
             </label>
-            <input type="number" className="w-full border p-2 rounded" /> */}
-            <label className="block mt-4 text-gray-400 text-sm">
-              Fecha de la venta
-            </label>
-            <input type="date" value={currentDate} className="w-full border p-2 mb-4 rounded text-black" />
-            <button className="bg-indigo-500 text-white px-4 py-2 mt-4 rounded w-full">
-              Crear Venta
+            <select className="w-full border p-2 rounded text-black mb-4" id="payment-method" value={paymentMethod} onChange={(e) => setPaymentMethod(e.target.value)}>
+              <option value="Efectivo">Efectivo</option>
+              <option value="TarjetaDC">Tarjeta Débito/Crédito</option>
+              <option value="Deposito">Depósito</option>
+              <option value="Transferencia">Transferencia</option>
+            </select>
+            <button className={carShop.length > 0 ? s["Cobranza-buttonPay"] : s["Cobranza-buttonDisable"]} disabled={carShop.length == 0}>
+              Pagar
             </button>
             <button className="bg-rose-500 text-white px-4 py-2 mt-4 rounded w-full" onClick={() => closeDrawerVenta()}>
               Cancelar
